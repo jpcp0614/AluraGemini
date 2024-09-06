fcccc47d6af2355f6e4a7c90a8e09738a792655c
João Paulo Pereira <jpcp0614@gmail.com>
2024-09-06 11:11:24 -0300

    add data

---
+++ data.js
@@ -0,0 +1,22 @@
+let dados = [
+	{
+		titulo: 'Rebeca Andrade',
+		descricao:
+			'É uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e determinação. Bicampeã olímpica, Rebeca acumula diversas medalhas em competições nacionais e internacionais, consolidando seu lugar como uma das principais atletas do mundo. Sua trajetória inspiradora, marcada por superação de lesões e desafios, a transformou em um ícone do esporte brasileiro.',
+		link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+	},
+	{
+		titulo: 'Rayssa Leal',
+		descricao:
+			'Conhecida como "Fadinha do Skate", Rayssa Leal é uma das atletas mais jovens e talentosas do Brasil. Aos 13 anos, já conquistou a medalha de prata nas Olimpíadas de Tóquio 2020, se tornando uma das principais referências do skate mundial. Com seu estilo único e carisma, Rayssa não só popularizou o esporte no Brasil como também se tornou um exemplo de perseverança e determinação para jovens em todo o mundo.',
+		link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+	},
+	{
+		titulo: 'Beatriz Souza',
+		descricao:
+			'Beatriz Sousa é uma das principais judocas do Brasil, conhecida por sua técnica apurada e espírito combativo. Ao longo de sua carreira, acumulou importantes conquistas em campeonatos nacionais e internacionais, se consolidando como uma das atletas mais promissoras do judô brasileiro. Com uma trajetória marcada por desafios e vitórias, Beatriz se destaca não apenas por suas habilidades no tatame, mas também por sua determinação e dedicação ao esporte.',
+		link: 'https://pt.wikipedia.org/wiki/Beatriz_Sousa',
+	},
+];
+
+export default dados;
