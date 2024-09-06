6845827621121034d0567e9cb1a953ac46af7a6f
João Paulo Pereira <jpcp0614@gmail.com>
2024-09-06 15:04:39 -0300

    add function pesquisar

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Hello World!');
+function pesquisar() {
+	let section = document.getElementById('resultados-pesquisa');
+	let campoPesquisa = document.getElementById('campo-pesquisa').value;
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Você não digitou nada</p>"
+    return;
+  };
+
+	let resultados = '';
+
+	for (let dado of dados) {
+		if (
+			dado.titulo.toLowerCase().includes(campoPesquisa) ||
+			dado.descricao.toLowerCase().includes(campoPesquisa)
+		) {
+			resultados += `
+      <div class="item-resultado">
+        <h2>${dado.titulo}</h2>
+        <p class="descricao-meta">
+          ${dado.descricao}
+        </p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+      </div>
+    `;
+		}
+	}
+
+  if (!resultados) {
+    section.innerHTML = "<p>Nenhum resultado encontrado</p>"
+    return;
+  }
+
+	section.innerHTML = resultados;
+}
